a7d2275b42af0f61516eb3601a0c7518454d018e
Rimvydas Riauba <rimvydor@gmail.com>
2017-04-12 19:55:28 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,18 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+
+public class Controller {
+
+
+    @FXML
+    private Label text;
+
+    public  void sayHi (ActionEvent event){
+       String prefix = text.getText();
+        text.setText(prefix + "Jonas");
+    }
+}
